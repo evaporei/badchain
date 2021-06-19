389613a4fabb3c1c09e954c7ed00042f31ee26d5
Otávio Pace <otaviopp8@gmail.com>
2021-06-19 00:10:43 -0300

    pow: Add ProofOfWork

--- src/block.rs
+++ src/block.rs
@@ -1,12 +1,12 @@
-use sha2::digest::Digest;
-use sha2::Sha256;
+use crate::pow::{self, ProofOfWork};
 use std::time::SystemTime;
 
 pub struct Block {
-    timestamp: u64,
+    pub timestamp: u64,
     pub data: Vec<u8>,
     pub prev_block_hash: Vec<u8>,
     pub hash: Vec<u8>,
+    pub nonce: u64,
 }
 
 fn unix_time() -> u64 {
@@ -18,27 +18,23 @@ fn unix_time() -> u64 {
 
 impl Block {
     pub fn new(data: &str, prev_block_hash: &[u8]) -> Self {
-        let mut block = Self {
+        let block = Self {
             timestamp: unix_time(),
             data: data.bytes().collect(),
             prev_block_hash: prev_block_hash.into(),
             hash: vec![],
+            nonce: 0,
         };
 
-        block.set_hash();
+        let pow = ProofOfWork::new(block);
+
+        let pow::Data { nonce, hash } = pow.run();
+
+        let ProofOfWork { mut block, .. } = pow;
+
+        block.hash = hash;
+        block.nonce = nonce;
 
         block
-    }
-
-    fn set_hash(&mut self) {
-        let timestamp: Vec<u8> = self.timestamp.to_string().bytes().collect();
-
-        let mut hasher = Sha256::new();
-        hasher.update(&self.prev_block_hash);
-        hasher.update(&self.data);
-        hasher.update(&timestamp);
-        let hash = hasher.finalize()[..].to_vec();
-
-        self.hash = hash;
     }
 }
